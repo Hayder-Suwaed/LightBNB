e1ee9cf31b70e7456f22462dd1c29096b8b5ec77
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-05 23:39:34 +0000

    most visited city

--- 1_queries/property_listings_city.sql
+++ 1_queries/property_listings_city.sql
@@ -1,7 +1,7 @@
 SELECT properties.*,
-avg(property_reviews.rating) as average_rating
+    avg(property_reviews.rating) as average_rating
 FROM properties
-JOIN property_reviews ON properties.id = property_id
+    JOIN property_reviews ON properties.id = property_id
 WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
 HAVING avg(property_reviews.rating) >= 4
