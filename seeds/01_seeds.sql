bea4dac4333f97f1eb5fcdb82356d5023f5e57ae
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-02-05 22:13:38 +0000

    Please enter the commit message for your changes. Lines starting create seed files write INSERT queries to add data to all of tables

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -2,20 +2,26 @@
 DELETE FROM users;
 DELETE FROM reservations;
 
-INSERT INTO users (name, email, password)
-VALUES ('brandon fake', 'fake1@hotmail.com'
+INSERT INTO users
+    (name, email, password)
+VALUES
+    ('brandon fake', 'fake1@hotmail.com'
 , '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.') ,
-('ayo fake', 'fake2@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('rusul fake', 'fake3@hotmai.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('property owner', 'iownproperty@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    ('ayo fake', 'fake2@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+    ('rusul fake', 'fake3@hotmai.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+    ('property owner', 'iownproperty@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO reservations (id, start_date, end_date)
-VALUES (1, '2018-09-11', '2018-09-26'),
-(2, '2019-01-04', '2019-02-01'),
-(3,  '2021-10-01', '2021-10-14');
+INSERT INTO reservations
+    (id, start_date, end_date)
+VALUES
+    (1, '2018-09-11', '2018-09-26'),
+    (2, '2019-01-04', '2019-02-01'),
+    (3, '2021-10-01', '2021-10-14');
 
 
-INSERT INTO properties ( title, description, thumbnail_photo_url, cover_photo_url,cost_per_night,parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street,  city , province ,post_code)
-VALUES ('title', 'description', 'thumb', 'cover', 20,1,1,1, 'canada', 75, 'fakestreet','toronto','on'),
-('title', 'description', 'thumb', 'cover', 20,1,1,1, 'canada', 85, 'fakestreet','toronto','on'),
-('title', 'description', 'thumb', 'cover', 20,1,1,1, 'canada', 95, 'fakestreet','toronto','on');
+INSERT INTO properties
+    ( title, description, thumbnail_photo_url, cover_photo_url,cost_per_night,parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city , province ,post_code)
+VALUES
+    ('title', 'description', 'thumb', 'cover', 20, 1, 1, 1, 'canada', 75, 'fakestreet', 'toronto', 'on'),
+    ('title', 'description', 'thumb', 'cover', 20, 1, 1, 1, 'canada', 85, 'fakestreet', 'toronto', 'on'),
+    ('title', 'description', 'thumb', 'cover', 20, 1, 1, 1, 'canada', 95, 'fakestreet', 'toronto', 'on');
